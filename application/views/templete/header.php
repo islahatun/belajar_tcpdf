a9dd904aa0005a443db46d628ae3c69274fb32ad
islahatun <islahatunnufusi98@gmail.com>
2021-09-13 01:24:29 +0700

    Menampilkan modal tambah data

--- application/views/templete/header.php
+++ application/views/templete/header.php
@@ -16,33 +16,17 @@
     <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">Daftar Hadir Siswa</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" data-bs-toggle="modal" data-bs-target="#tambahsiswa">Tambah Siswa</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            <a class="nav-link" href="#">Cetak Laporan</a>
                         </li>
                     </ul>
                     <form class="d-flex">
@@ -52,4 +36,56 @@
                 </div>
             </div>
         </nav>
+    </div>
+
+    <!-- Button trigger modal -->
+    <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#tambahsiswa">
+        Launch demo modal
+    </button> -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="tambahsiswa" tabindex="-1" aria-labelledby="tambahsiswaLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="tambahsiswaLabel">Tambah Siswa</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="<?= base_url('Siswa/tambah') ?>">
+                        <div class="row g-3 align-items-center">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="col-auto">
+                                        <label for="inputPassword6" class="col-form-label">NISN</label>
+                                    </div>
+                                    <div class="col-auto">
+                                        <input type="text" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="col-auto">
+                                        <label for="inputPassword6" class="col-form-label">NIS</label>
+                                    </div>
+                                    <div class="col-auto">
+                                        <input type="text" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="col-auto">
+                                        <label for="inputPassword6" class="col-form-label">NAMA</label>
+                                    </div>
+                                    <div class="col-auto">
+                                        <input type="text" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+                </form>
+            </div>
+        </div>
     </div>
